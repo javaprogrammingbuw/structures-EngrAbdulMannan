3d8b1774540baebd29f2257e301cbba1e46e9a92
abdulmannan <abdulmannancc33@gmail.com>
2019-03-18 15:34:12 +0100

    even odd exercise done

--- EvenOdd.java
+++ EvenOdd.java
@@ -10,6 +10,14 @@ public class EvenOdd {
 		//todo: The program takes a number as input.
 		//It then goes through all integer values till reaching that value and prints all the odd ones.
 		
+		System.out.print("Enter a number ");
+		int n =scan.nextInt();
+	for (int i =0;i<n;i++)
+	{
+		if (i%2!=0) {
+			System.out.println("Odd nos are "+i);
+		}
+	}
 	}
 	
 }
